7518ee948f8c57b110a73be229e01b48c0324728
Igor <musichound.ge@gmail.com>
2025-02-20 23:13:44 +0500

    minor fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Movie-app PWA',
+        short_name: 'Movie-app',
+      },
+    })
+  ],
+  base: '/movie-app',
 })
